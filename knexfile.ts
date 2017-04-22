4c3f9617b1c55ac873c3bef06df584d0ba37a1c8
David Sudia <dsudia@gmail.com>
2017-04-22 03:48:42 -0600

    add basic file structure

---
+++ knexfile.ts
@@ -0,0 +1,10 @@
+export const config =  {
+    development: {
+        client: 'pg',
+        connection: 'postgres://localhost:5432/mm'
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
+    }
+}
