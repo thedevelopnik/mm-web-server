962701f906ac0788ba4575ccd40522a4db30b794
David Sudia <dsudia@fanatics.com>
2017-06-23 14:48:10 -0600

    align build systems of frontend and server

--- src/App.ts
+++ src/App.ts
@@ -8,8 +8,6 @@ import * as jwt from 'express-jwt';
 import { authRouter } from './v1-routes/auth';
 import { InvalidToken } from './v1-routes/errors';
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001'];
-
 class App {
     public express: express.Application;
 
@@ -21,25 +19,18 @@ class App {
 
     private middleware(): void {
         this.express.use(logger('dev'));
-        this.express.use(cors({
-            origin (origin: string, callback: Function) {
-                if (whitelist.includes(origin)) {
-                    callback(null, true);
-                } else {
-                    callback(new Error('Not allowed by CORS'));
-                }
-            },
-            credentials: true
-        }));
+        this.express.use(cors())
         this.express.use(bodyParser.json());
         this.express.use(bodyParser.urlencoded({ extended: false}));
         this.express.use(bodyParser.urlencoded({ extended: false }));
+        this.express.use('/', express.static(path.join(__dirname, 'client')));
         this.express.use(jwt({
             secret: process.env.JWT_SECRET || 'shhhh',
             maxAge: '1 day'
         }).unless({
+            ext: ['.html', '.js', '.ico', '.png'],
             path: [
-                '/index.html',
+                '/',
                 { url: '/auth', methods: ['GET', 'POST'] }
             ]
         }));
@@ -47,15 +38,17 @@ class App {
 
     private routes(): void {
         const router = express.Router();
-        router.get('/', (req, res) => {
-            res.sendFile(path.join(__dirname, '../build/index.html'));
-        });
-        this.express.use('/', router);
+        this.express.use('/api/v1', router);
         this.express.use('/api/v1/auth', authRouter);
         this.express.use(function(err: Error, req: Request, res: Response, next: NextFunction) {
             if (err.name === 'UnauthorizedError') {
-                res.status(401).send(InvalidToken);
+                res.status(401).json(InvalidToken);
                 next();
+            } else {
+                res.status(500).json({
+                    code: 5000,
+                    message: 'Unknown server error, please contact support @ hello@montessorimatch.com'
+                })
             }
         });
     }
